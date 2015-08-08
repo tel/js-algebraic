413aac2d42d686017c5ee49816379d4f2148e2b0
Joseph Abrahamson <me@jspha.com>
2015-08-07 22:13:16 -0400

    bump: ver 0.0.10

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "reify:algebraic",
-  version: "0.0.9",
+  version: "0.0.10",
   summary: "Algebraic module specification for Javascript",
   git: "",
   documentation: "README.md",
